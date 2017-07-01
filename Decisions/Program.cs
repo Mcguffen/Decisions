e521399dce51232b2f8c925f0d945734e6fd5d62
善阳 韩 <504177380@qq.com>
2017-07-01 11:40:24 +0800

    if判断

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -10,6 +10,38 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
+            Console.WriteLine("hsy's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3:");
+            string userValue = Console.ReadLine();
+
+            string message = "";
+
+
+            if (userValue == "1")
+                message = "You won a car!";
+            else if (userValue == "2")
+                message = "You won a new boat";
+            else if (userValue == "3")
+
+                message = "You won a new cat!";
+
+            else
+            {
+                message = "Sorry,we don't understand.";
+
+                //message = message + "You lose.";
+                message += "You lose.";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+            */
+            Console.WriteLine("hsy's Big Giveaway");
+            Console.Write("Choose a door: 1, 2 or 3:");
+            string userValue = Console.ReadLine();
+
+
+
         }
     }
 }
