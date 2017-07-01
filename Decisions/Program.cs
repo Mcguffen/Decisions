84c7aa5efba617beecfa6928da7237bd553c90e2
善阳 韩 <504177380@qq.com>
2017-07-01 12:14:19 +0800

    if判断语句if ,else if , else

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -40,6 +40,15 @@ namespace Decisions
             Console.Write("Choose a door: 1, 2 or 3:");
             string userValue = Console.ReadLine();
 
+            string message = (userValue=="1") ? "boat" : "stand of lint";
+
+            //Console.WriteLine("You won a ");
+            //Console.WriteLine(message);
+            //Console.Write(".");
+            //Console.ReadLine();
+
+            Console.WriteLine("You entered: {0},therefore you won a {1}.",userValue, message);
+            Console.ReadLine();
 
 
         }
